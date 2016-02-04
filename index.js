9d59f4b0cb176e7467e3f367048fdbe4b6aab5ca
Arne Schlüter <aesthaddicts@gmail.com>
2016-02-04 16:03:50 +0100

    Fix for files that have no last modification date

--- index.js
+++ index.js
@@ -635,30 +635,34 @@ function _xml_parse( dom ) {
     return dom.multistatus.response.map( function ( response ) {
       var href = response.href[0]
       var propstat = response.propstat[0]
-      var prop = response.propstat[0].prop[0]
-      var getlastmodified = response.propstat[0].prop[0].getlastmodified[0]
-      var stat = response.propstat[0].status[0]
       var resource = {}
       resource.href = href._
       if ( propstat ) {
+        var prop = propstat.prop ? propstat.prop[0] : null
+
         if ( prop ) {
-          var getcontentlength = response.propstat[0].prop[0].getcontentlength
+          var getlastmodified = prop.getlastmodified ? prop.getlastmodified[0] : Date.now()
+          var getcontentlength = prop ? prop.getcontentlength : null
+
           if ( getcontentlength ) {
             resource.getcontentlength = getcontentlength[0]._
           }
+
           if ( getlastmodified ) {
             resource.getlastmodified = new Date( getlastmodified._ )
           }
-          var resourcetype = response.propstat[0].prop[0].resourcetype
+
+          var resourcetype = prop.resourcetype
           if ( resourcetype ) {
             if ( typeof resourcetype[0] === 'string' ) {
               resource.resourcetype = null
-            } else
-            if ( typeof resourcetype[0] === 'object' ) {
+            } else if ( typeof resourcetype[0] === 'object' ) {
               resource.resourcetype = Object.keys( resourcetype[0] )[0]
             }
           }
         }
+
+        var stat = propstat.status ? propstat.status[0] : null
         if ( stat ) {
           resource.stat = http_status_to_int( stat._ )
         }
